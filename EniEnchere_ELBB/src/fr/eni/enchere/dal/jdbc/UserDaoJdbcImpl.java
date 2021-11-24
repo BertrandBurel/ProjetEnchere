89d08438eabfaf8e1d85631b0c9f80b89f5efc5a
BertrandBurel <legionxful@gmail.com>
2021-11-24 09:13:20 +0100

    add selectById() in UserDaoJdbcImpl

--- EniEnchere_ELBB/src/fr/eni/enchere/dal/jdbc/UserDaoJdbcImpl.java
+++ EniEnchere_ELBB/src/fr/eni/enchere/dal/jdbc/UserDaoJdbcImpl.java
@@ -13,6 +13,7 @@ import fr.eni.enchere.dal.DAO;
 public class UserDaoJdbcImpl implements DAO<User> {
 
 	private static final String SELECT_ALL_USERS = "select no_utilisateur, pseudo, email, mot_de_passe from UTILISATEURS";
+	private static final String SELECT_USER_BY_ID = "select * from UTILISATEURS where no_utilisateur = ?";
 	
 	@Override
 	public void insert(User t) {
@@ -22,7 +23,30 @@ public class UserDaoJdbcImpl implements DAO<User> {
 
 	@Override
 	public User selectById(int index) {
-		// TODO Auto-generated method stub
+		User user = new User();
+		try {
+			Connection cnx = ConnectionProvider.getConnection();
+			PreparedStatement pstmt = cnx.prepareStatement(SELECT_USER_BY_ID);
+			pstmt.setInt(1, index);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				user.setId(rs.getInt("no_utilisateur"));
+				user.setPseudonym(rs.getString("pseudo"));
+				user.setLastName(rs.getString("nom"));
+				user.setFirstName(rs.getString("prenom"));
+				user.setEmail(rs.getString("email"));
+				user.setPhone(rs.getString("telephone"));
+				user.setAddress(rs.getString("rue"));
+				user.setPostalCode(rs.getString("code_postal"));
+				user.setCity(rs.getString("ville"));
+				user.setPassword(rs.getString("mot_de_passe"));
+				user.setCredit(rs.getInt("credit"));
+				user.setAdministrator(rs.getBoolean("administrateur"));
+			}
+			cnx.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
@@ -52,6 +76,14 @@ public class UserDaoJdbcImpl implements DAO<User> {
 	public void delete(int index) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public User selectByPseudo(String pseudo) {
+		return null;
+	}
+	
+	public User selectByEmail(String email) {
+		return null;
 	}
 
 }
