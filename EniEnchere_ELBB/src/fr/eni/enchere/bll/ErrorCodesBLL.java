c767fa8515537f265857f108d4b67147b6de46df
BertrandBurel <legionxful@gmail.com>
2021-11-29 17:03:06 +0100

    BLL validation user pseudo

--- EniEnchere_ELBB/src/fr/eni/enchere/bll/ErrorCodesBLL.java
+++ EniEnchere_ELBB/src/fr/eni/enchere/bll/ErrorCodesBLL.java
@@ -2,4 +2,7 @@ package fr.eni.enchere.bll;
 
 public abstract class ErrorCodesBLL {
 
+	public static final int PSEUDO_LENGTH_ERROR = 20000;
+	
+	public static final int PSEUDO_REGEX_ERROR = 20001;
 }
--- EniEnchere_ELBB/src/fr/eni/enchere/view/ServletCreateAccount.java
+++ EniEnchere_ELBB/src/fr/eni/enchere/view/ServletCreateAccount.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import fr.eni.enchere.bll.UserManager;
 
 /**
  * Servlet implementation class ServletCreateAccount
