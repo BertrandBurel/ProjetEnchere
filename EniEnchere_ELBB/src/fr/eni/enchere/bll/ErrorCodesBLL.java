c57cfe53a8f388cba617c753f2938fc2aec26a06
BertrandBurel <legionxful@gmail.com>
2021-11-30 09:26:05 +0100

    Add BLL error messages

--- EniEnchere_ELBB/src/fr/eni/enchere/bll/ErrorCodesBLL.java
+++ EniEnchere_ELBB/src/fr/eni/enchere/bll/ErrorCodesBLL.java
@@ -3,6 +3,29 @@ package fr.eni.enchere.bll;
 public abstract class ErrorCodesBLL {
 
 	public static final int PSEUDO_LENGTH_ERROR = 20000;
-	
 	public static final int PSEUDO_REGEX_ERROR = 20001;
+	
+	public static final int LASTNAME_LENGTH_ERROR = 20002;
+	public static final int LASTNAME_REGEX_ERROR = 20003;
+	
+	public static final int FIRSTSTNAME_LENGTH_ERROR = 20004;
+	public static final int FIRSTSTNAME_REGEX_ERROR = 20005;
+	
+	public static final int EMAIL_LENGTH_ERROR = 20006;
+	public static final int EMAIL_REGEX_ERROR = 20007;
+	
+	public static final int PHONE_LENGTH_ERROR = 20008;
+	public static final int PHONE_REGEX_ERROR = 20009;
+	
+	public static final int ADDRESS_LENGTH_ERROR = 20010;
+	public static final int ADDRESS_REGEX_ERROR = 20011;
+	
+	public static final int POSTALCODE_LENGTH_ERROR = 20012;
+	public static final int POSTALCODE_REGEX_ERROR = 20013;
+	
+	public static final int CITY_LENGTH_ERROR = 20014;
+	public static final int CITY_REGEX_ERROR = 20015;
+	
+	public static final int PASSWORD_LENGTH_ERROR = 20016;
+	public static final int CONFIRM_PASSWORD_ERROR = 20017;
 }
